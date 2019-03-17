3213741c7d244d69b413330a9e9d382f3c5217c7
pi3rrot <4dakto@gmail.com>
2019-03-17 19:35:58 +0900

    auth

--- router/main.js
+++ router/main.js
@@ -1,26 +1,29 @@
 module.exports = (app, fs) => {
-  app.get('/', (req, res) => {
-    res.render('index', {
-      title: 'MY HOMEPAGE',
-      length: 5
+  app.get("/", (req, res) => {
+    const sess = req.session;
+
+    res.render("index", {
+      title: "MY HOMEPAGE",
+      length: 5,
+      name: sess.name,
+      username: sess.username
     });
   });
 
-  app.get('/list', (req, res) => {
-    fs.readFile(__dirname + "/../data/" + "user.json", 'utf8', (err, data) => {
+  app.get("/list", (req, res) => {
+    fs.readFile(__dirname + "/../data/" + "user.json", "utf8", (err, data) => {
       res.end(data);
     });
   });
 
-  app.get('/getUser/:username', function (req, res) {
-    fs.readFile(__dirname + "/../data/user.json", 'utf8', (err, data) => {
+  app.get("/getUser/:username", function(req, res) {
+    fs.readFile(__dirname + "/../data/user.json", "utf8", (err, data) => {
       const users = JSON.parse(data);
       res.json(users[req.params.username]);
     });
   });
 
-  app.post('/addUser/:username', function (req, res) {
-
+  app.post("/addUser/:username", function(req, res) {
     let result = {};
     const username = req.params.username;
 
@@ -33,7 +36,7 @@ module.exports = (app, fs) => {
     }
 
     // LOAD DATA & CHECK DUPLICATION
-    fs.readFile(__dirname + "/../data/user.json", 'utf8', function (err, data) {
+    fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data) {
       const users = JSON.parse(data);
       if (users[username]) {
         // DUPLICATION FOUND
@@ -47,16 +50,19 @@ module.exports = (app, fs) => {
       users[username] = req.body;
 
       // SAVE DATA
-      fs.writeFile(__dirname + "/../data/user.json",
-        JSON.stringify(users, null, '\t'), "utf8", function (err, data) {
-          result = { "success": 1 };
+      fs.writeFile(
+        __dirname + "/../data/user.json",
+        JSON.stringify(users, null, "\t"),
+        "utf8",
+        function(err, data) {
+          result = { success: 1 };
           res.json(result);
-        })
-    })
+        }
+      );
+    });
   });
 
-  app.put('/updateUser/:username', function (req, res) {
-
+  app.put("/updateUser/:username", function(req, res) {
     let result = {};
     const username = req.params.username;
 
@@ -69,24 +75,28 @@ module.exports = (app, fs) => {
     }
 
     // LOAD DATA
-    fs.readFile(__dirname + "/../data/user.json", 'utf8', function (err, data) {
+    fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data) {
       const users = JSON.parse(data);
       // ADD/MODIFY DATA
       users[username] = req.body;
 
       // SAVE DATA
-      fs.writeFile(__dirname + "/../data/user.json",
-        JSON.stringify(users, null, '\t'), "utf8", function (err, data) {
-          result = { "success": 1 };
+      fs.writeFile(
+        __dirname + "/../data/user.json",
+        JSON.stringify(users, null, "\t"),
+        "utf8",
+        function(err, data) {
+          result = { success: 1 };
           res.json(result);
-        })
-    })
+        }
+      );
+    });
   });
 
-  app.delete('/deleteUser/:username', function (req, res) {
+  app.delete("/deleteUser/:username", function(req, res) {
     let result = {};
     //LOAD DATA
-    fs.readFile(__dirname + "/../data/user.json", "utf8", function (err, data) {
+    fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data) {
       const users = JSON.parse(data);
 
       // IF NOT FOUND
@@ -101,13 +111,61 @@ module.exports = (app, fs) => {
       delete users[req.params.username];
 
       // SAVE FILE
-      fs.writeFile(__dirname + "/../data/user.json",
-        JSON.stringify(users, null, '\t'), "utf8", function (err, data) {
+      fs.writeFile(
+        __dirname + "/../data/user.json",
+        JSON.stringify(users, null, "\t"),
+        "utf8",
+        function(err, data) {
           result["success"] = 1;
           res.json(result);
           return;
-        })
-    })
+        }
+      );
+    });
+  });
 
-  })
+  app.get("/login/:username/:password", function(req, res) {
+    const sess;
+    sess = req.session;
+
+    fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data) {
+      const users = JSON.parse(data);
+      const username = req.params.username;
+      const password = req.params.password;
+      const result = {};
+      if (!users[username]) {
+        // USERNAME NOT FOUND
+        result["success"] = 0;
+        result["error"] = "not found";
+        res.json(result);
+        return;
+      }
+
+      if (users[username]["password"] == password) {
+        result["success"] = 1;
+        sess.username = username;
+        sess.name = users[username]["name"];
+        res.json(result);
+      } else {
+        result["success"] = 0;
+        result["error"] = "incorrect";
+        res.json(result);
+      }
+    });
+  });
+
+  app.get("/logout", function(req, res) {
+    const sess = req.session;
+    if (sess.username) {
+      req.session.destroy(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect("/");
+        }
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
 };
